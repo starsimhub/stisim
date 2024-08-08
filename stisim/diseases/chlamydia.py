095e84fa4a0bfaa23dfa316933252c77986cd94d
Robyn Stuart <robyna.s@gmail.com>
2024-08-08 15:41:02 +1000

    add devtest

--- stisim/diseases/chlamydia.py
+++ stisim/diseases/chlamydia.py
@@ -62,8 +62,6 @@ class Chlamydia(ss.Infection):
             self.pars.beta['structuredsexual'][0] *= self.pars.beta_m2f
         if self.pars.beta_f2m is not None:
             self.pars.beta['structuredsexual'][1] *= self.pars.beta_f2m
-        if self.pars.beta_m2c is not None:
-            self.pars.beta['maternal'][0] *= self.pars.beta_m2c
         return
 
     def init_post(self):
