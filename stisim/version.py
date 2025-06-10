806bbcf92289e0f75b241324444df3d1ea8d4c1d
Robyn Stuart <robyna.s@gmail.com>
2025-06-10 19:33:27 +1000

    base branch

--- stisim/version.py
+++ stisim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '1.2.0'
-__versiondate__ = '2025-06-10'
+__version__ = '1.3.0'
+__versiondate__ = '2025-06-20'
 __license__ = f'STIsim {__version__} ({__versiondate__}) — © 2024-2025 by IDM'
