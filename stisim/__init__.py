2173f504d6ac70ddf31d34e457f062670d146d41
Robyn Stuart <robyna.s@gmail.com>
2025-08-15 14:27:37 +1000

    temp pin

--- stisim/__init__.py
+++ stisim/__init__.py
@@ -20,6 +20,6 @@ data = root/'data'
 print(__license__)
 
 # Double-check key requirements -- should match setup.py
-sc.require(['starsim==2.2.3', 'sciris>=3.1.6', 'pandas>=2.0.0', 'scipy', 'numba', 'networkx'], message=f'The following dependencies for STIsim {__version__} were not met: <MISSING>.')
+sc.require(['starsim==2.3.2', 'sciris>=3.1.6', 'pandas>=2.0.0', 'scipy', 'numba', 'networkx'], message=f'The following dependencies for STIsim {__version__} were not met: <MISSING>.')
 del sc # Don't keep this in the module
 
