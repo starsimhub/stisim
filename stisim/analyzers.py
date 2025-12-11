16baaaa9f0a53fb5fbd04a4453cf05863be529d3
Robyn Stuart <robyna.s@gmail.com>
2025-12-12 07:04:07 +1100

    remove line

--- stisim/analyzers.py
+++ stisim/analyzers.py
@@ -41,7 +41,7 @@ class coinfection_stats(result_grouper):
     def __init__(self, disease1, disease2, disease1_infected_state_name='infected', disease2_infected_state_name='infected',
                  age_limits=None, denom=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.name = 'coinfection_stats'
+        # self.name = name or 'coinfection_stats'
         if disease1 is None or disease2 is None:
             raise ValueError('Coinfection stats requires exactly 2 diseases')
 
