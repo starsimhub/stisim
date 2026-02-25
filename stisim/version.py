8183b10be1153099bd45bc00e8322460bcc1d8bb
Robyn Stuart <robyna.s@gmail.com>
2026-02-25 08:29:26 -0800

    update date

--- stisim/version.py
+++ stisim/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '1.4.9'
-__versiondate__ = '2026-02-24'
+__versiondate__ = '2026-02-25'
 __license__ = f'STIsim {__version__} ({__versiondate__}) — © 2024-2026 by IDM'
