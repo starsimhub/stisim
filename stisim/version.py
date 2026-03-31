c5291fd26ea7685e186d6bc40f7339984b599299
Robyn Stuart <robyna.s@gmail.com>
2026-03-31 11:33:38 -0700

    Pin starsim>=3.3.0 and sciris>=3.2.9, update version date to 2026-03-31

--- stisim/version.py
+++ stisim/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '1.5.2'
-__versiondate__ = '2026-03-27'
+__versiondate__ = '2026-03-31'
 __license__ = f'STIsim {__version__} ({__versiondate__}) — © 2024-2026 by IDM'
