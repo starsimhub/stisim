b78ec2403befb6bafeaf3b8bda28cc36328062aa
Robyn Stuart <robyna.s@gmail.com>
2026-04-06 12:40:49 -0700

    Move test_hiv_epi from test_stis to test_hiv, rename to test_par_ranges

--- tests/test_stis.py
+++ tests/test_stis.py
@@ -61,46 +61,6 @@ def test_syph_epi():
     return s0, s1
 
 
-def test_hiv_epi():
-    """ Test that HIV epi parameters affect dynamics in the expected direction """
-    sc.heading('Test epi dynamics of hiv')
-
-    base_pars = dict(n_agents=n_agents, diseases='hiv', beta_m2f=0.05, init_prev=0.05)
-
-    # Define the parameters to vary
-    par_effects = dict(
-        beta_m2f=[0.01, 0.2],
-    )
-
-    # Loop over each of the above parameters and make sure they affect the epi dynamics in the expected ways
-    for par, par_val in par_effects.items():
-        lo = par_val[0]
-        hi = par_val[1]
-
-        # Make baseline pars
-        pars0 = sc.dcp(base_pars)
-        pars1 = sc.dcp(base_pars)
-
-        pars0[par] = lo
-        pars1[par] = hi
-
-        # Run the simulations and pull out the results
-        s0 = sti.Sim(pars0, label=f'{par} {par_val[0]}')
-        s1 = sti.Sim(pars1, label=f'{par} {par_val[1]}')
-        ss.parallel(s0, s1, debug=debug)
-
-        # Check results
-        ind = 1 if par == 'init_prev' else -1
-        v0 = s0.results.hiv.cum_infections[ind]
-        v1 = s1.results.hiv.cum_infections[ind]
-
-        print(f'Checking with varying {par:10s} ... ', end='')
-        assert v0 <= v1, f'Expected infections to be lower with {par}={lo} than with {par}={hi}, but {v0} > {v1})'
-        print(f'✓ ({v0} <= {v1})')
-
-    return s0, s1
-
-
 def test_stis(which='discharging', n_agents=5e3, start=2010, stop=2020):
     """ Test running grouped STIs: discharging (NG, CT, TV, BV) and ulcerative (syphilis, GUD) """
     sc.heading('Test STI sim')
@@ -192,7 +152,6 @@ def test_bv(include_hiv=False, n_agents=500):
 if __name__ == '__main__':
     sc.options(interactive=False)
     s1, s2 = test_syph_epi()
-    s3, s4 = test_hiv_epi()
     test_stis(which='discharging')
     test_stis(which='ulcerative')
     test_bv()
