f2054a316bd13d2d83cdb6ad5f28b4672647d51a
Robyn Stuart <robyna.s@gmail.com>
2026-05-04 09:14:31 -0700

    Bump version to 1.5.4 (2026-05-04); update CHANGELOG

--- stisim/version.py
+++ stisim/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '1.5.4'
-__versiondate__ = '2026-04-17'
+__versiondate__ = '2026-05-04'
 __license__ = f'STIsim {__version__} ({__versiondate__}) — © 2024-2026 by IDM'
