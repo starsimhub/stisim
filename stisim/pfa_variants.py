04f0558f92acfd29f1e62e9f1f275fd1bd772c3d
Robyn Stuart <robyna.s@gmail.com>
2026-05-15 10:30:17 -0700

    feat: add SortPair PFA variant

--- stisim/pfa_variants.py
+++ stisim/pfa_variants.py
@@ -26,3 +26,23 @@ class MFNetwork_LSA(MFNetwork):
         p1 = m_eligible.uids[ind_m]
         p2 = f_looking[ind_f]
         return p1, p2
+
+
+class MFNetwork_SortPair(MFNetwork):
+    """argsort both groups, zip, truncate to min(len). No tail trim."""
+
+    def match_pairs(self):
+        ppl = self.sim.people
+        f_looking, m_eligible = self._get_eligible()
+        desired_ages = self._sample_desired_ages(f_looking)
+        m_ages = ppl.age[m_eligible]
+        ind_m = np.argsort(m_ages, stable=True)
+        ind_f = np.argsort(desired_ages, stable=True)
+        maxlen = min(len(ind_m), len(ind_f))
+        if maxlen == 0:
+            raise NoPartnersFound()
+        ind_m = ind_m[:maxlen]
+        ind_f = ind_f[:maxlen]
+        p1 = m_eligible.uids[ind_m]
+        p2 = f_looking[ind_f]
+        return p1, p2
