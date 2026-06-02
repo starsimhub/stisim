4abd8816500334e04771126529080b2f8fb6bfb4
Robyn Stuart <robyna.s@gmail.com>
2026-06-02 16:26:29 -0700

    Open rc1.5.7: bump version, stub CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 All notable changes to the codebase are documented in this file.
 
+## Version 1.5.7 (TBC)
+
 ## Version 1.5.6 (2026-06-02)
 
 ### Networks
--- stisim/version.py
+++ stisim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '1.5.6'
+__version__ = '1.5.7'
 __versiondate__ = '2026-06-02'
 __license__ = f'STIsim {__version__} ({__versiondate__}) — © 2024-2026 by IDM'
