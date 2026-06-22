608aa64cb771b1c6b93d788c42c58c2301bb7720
ckirkman-IDM <ckirkman@idmod.org>
2026-06-22 12:32:16 -0700

    Logistics (#492)
    * adding logistics module for supply tracking of 1+ supplies of 1+ (arbitrary) products
    * New logistics classes: SuppliedIntervention, Supplies, Supply, Product
    * Supplies objects can be shared between SuppliedIntervention instances to create inter-intervention limits/supply limits.

---
+++ stisim/logistics/product_category.py
@@ -0,0 +1,16 @@
+"""Define ProductCategory: the category enum used to group Products by function."""
+from enum import Enum
+
+__all__ = ["ProductCategory"]
+
+
+class ProductCategory(Enum):
+    """
+    Category of a Product, used to determine if 1+ Products have a similar function (for example,
+    ProductCategory.PREP applies to both oral and injectable PrEP).
+
+    Pass members directly (ProductCategory.PREP); raw string values are not accepted.
+    """
+    PREP = 'prep'
+    ART = 'art'
+    BARRIER = 'barrier'
