8c16bdda7fb3872e24db0feb9881a3be3427b4c7
Robyn Stuart <robyna.s@gmail.com>
2026-06-25 21:47:27 -0700

    Open rc1.5.9: bump version, stub CHANGELOG

--- stisim/version.py
+++ stisim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '1.5.8'
+__version__ = '1.5.9'
 __versiondate__ = '2026-06-25'
 __license__ = f'STIsim {__version__} ({__versiondate__}) — © 2024-2026 by IDM'
